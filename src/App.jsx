410acde14476d6d6b587b520353f9872bcb50976
Wasif Hasan <wasifhasancse@gmail.com>
2026-03-30 13:03:29 +0600

    complete the NavBar as Figma with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
-      <div><h1 className='text-3xl text-red-500'>Welcome to Vite + React</h1></div>
+      <NavBar/>
     </>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,128 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="pr-2.5 lg:hidden pl-0 hover:text-violet-600 transition-colors duration-300"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Products</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Features</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Pricing</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Testimonials</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>FAQ</a>
+            </li>
+            </ul>
+          </div>
+          <a className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent text-3xl font-black">
+            DigiTools
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-lg font-medium">
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Products</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Features</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Pricing</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>Testimonials</a>
+            </li>
+            <li className="hover:text-violet-600 transition-colors duration-300">
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-4">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle transition-colors duration-300 hover:text-violet-600"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />{" "}
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+            >
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center gap-4">
+            <button className="text-lg font-semibold hover:text-violet-600 transition-colors duration-300">
+              Login
+            </button>
+            <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-lg text-white font-semibold hover:from-[#9514FA] hover:to-[#4F39F6] transition-colors duration-500">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
