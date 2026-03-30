cdc70090b43c0491d15becc9e491c8b632bd0694
Wasif Hasan <wasifhasancse@gmail.com>
2026-03-30 12:18:55 +0600

    initialization of B13-A6-DigiTools-Platform

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div><h1 className='text-3xl text-red-500'>Welcome to Vite + React</h1></div>
+    </>
+  )
+}
+
+export default App
